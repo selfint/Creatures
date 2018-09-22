0db3f82d817265ff95787d72e720c3c38b21aa4a
tomaster777 <selfint@gmail.com>
2018-09-22 18:02:39 +0300

    Continues to isolate graphics.
    Moved graphics to Graphics object, that handles pygame. simulation.py is the old main.py, the new one combines graphics.py with simulation.py

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from graphics import Graphics
 from simulation import Simulation
 
 # Constants
+HEIGHT = 600
+WIDTH = 800
+CAPTION = 'Creatures'
 
 if __name__ == '__main__':
     s = Simulation(1, 2, 1)
-    g = Graphics(s)
+    g = Graphics(s, width=WIDTH, height=HEIGHT, caption=CAPTION)
     g.run()
