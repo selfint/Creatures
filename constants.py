c85475976f71e7f7a0c271391b90b2f5347368be
tomaster777 <selfint@gmail.com>
2018-09-23 11:15:32 +0300

    Begins shifting ALL constants into constants.py

--- constants.py
+++ constants.py
@@ -6,13 +6,23 @@ from typing import Union, Tuple
 
 from node import InputNode, HiddenNode, OutputNode
 
+WIDTH, HEIGHT = 800, 600
+CAPTION = 'Creatures'
+POPULATION_SIZE = 2
+CREATURE_INPUTS = 2
+CREATURE_OUTPUTS = 4
+
 Node = Union[InputNode,HiddenNode,OutputNode]
 COLOR = Tuple[float, float, float]
 CreatureInfo = collections.namedtuple('CreatureInfo', 'x y scale')
+
+# Network input to make decision for a CREATURE (for other object another network is needed).
+CreatureNetworkInput = collections.namedtuple('NetworkInput', 'dx dy')
 
 CREATURE_COLORS = {
 'RED' : (255, 0, 0),
 'GREEN' : (0, 255, 0),
 'BLUE' : (0, 0, 255),
-'PINK': (255, 0, 255)
+'PINK': (255, 0, 255),
+'CYAN': (0, 255, 255)
 }
--- main.py
+++ main.py
@@ -7,15 +7,15 @@ from graphics import Graphics
 from simulation import Simulation
 
 # Constants
-HEIGHT = 600
-WIDTH = 800
-CAPTION = 'Creatures'
-POPULATION_SIZE = 2
-CREATURE_INPUTS = 2
-CREATURE_OUTPUTS = 4
+from constants import CAPTION, CREATURE_INPUTS, CREATURE_OUTPUTS, HEIGHT, POPULATION_SIZE, WIDTH
 
 
 if __name__ == '__main__':
     simulation = Simulation(POPULATION_SIZE, CREATURE_INPUTS, CREATURE_OUTPUTS)
+    creature1, creature2 = simulation.population.keys()
+    c1, c2 = simulation.population.values()
+    temp1 = simulation.info_to_vec(c1, c2)
+    temp2 = simulation.info_to_vec(c2, c1)
+    print(creature1.think(temp1))
     graphics = Graphics(simulation, WIDTH, HEIGHT, CAPTION)
     graphics.run()
