45a2b5531388fb861f6b3859dcf43479dd4999a6
tomaster777 <selfint@gmail.com>
2018-09-25 23:35:05 +0300

    Adds more parameters.

--- Constants/neat_parameters.py
+++ Constants/neat_parameters.py
@@ -2,10 +2,20 @@
 # Description: neat algorithm configurations.
 # ---------------------------------------------------------------------------------------------------------------------
 
+# General
 POPULATION_SIZE = 2
 CREATURE_INPUTS = 2
 CREATURE_OUTPUTS = 5
+
+# Weights
 WEIGHT_RANGE = 2.0
-BIAS_RANGE = 2.0
 WEIGHT_MUTATION_RATE = 0.2
 WEIGHT_PERTRUB_RATE = 0.1
+WEIGHT_PERTRUB_AMOUNT = 0.1
+
+# Bias
+BIAS_RANGE = 2.0
+BIAS_MUTATION_RATE = 0.2
+BIAS_PERTRUB_RATE = 0.2
+BIAS_PERTRUB_AMOUNT = 0.1
+
