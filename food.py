b8d7204aea0fb4f90a6da65e35ed43200dce487b
tomaster777 <selfint@gmail.com>
2018-12-09 18:37:01 +0200

    Started work on food object

---
+++ food.py
@@ -0,0 +1,15 @@
+# food.py
+# Description: food object for simulation.
+# ---------------------------------------------------------------------------------------
+
+# Imports
+
+
+# Constants
+
+
+class Food:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
