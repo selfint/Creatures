6ced7aaa44e94ea014d4361a6716f0787f56578a
tomaster777 <selfint@gmail.com>
2018-12-10 22:05:30 +0200

    Main prompts user to choose text or graphical mode simulation.

--- main.py
+++ main.py
@@ -12,7 +12,14 @@ from simulation import Simulation
 if __name__ == '__main__':
     simulation = Simulation()
     graphics = Graphics(simulation)
-    graphics.graphical_run()
+
+    mode = input('Which mode to run (1-graphical, 2-textual) ')
+    if mode == '1':
+        graphics.graphical_run()
+    elif mode == '2':
+        graphics.text_run()
+    else:
+        raise ValueError(mode, "is not acceptable.")
     sys.exit(1)
 
 
